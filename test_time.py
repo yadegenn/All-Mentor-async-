78d5d77a0c5d559767499c86555a3a3e1ec3c2f7
yadegenn <yadegenn@gmail.com>
2026-02-08 16:36:28 +0500

    rm trash

--- test_time.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timedelta, time
-
-current_hour = datetime.combine((datetime.now() + timedelta(days=1)).date(), time())
-rule_hour = 20
-
-print(current_hour)
-if(current_hour.hour>=rule_hour):
-    print("сработало")
